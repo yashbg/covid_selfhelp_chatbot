fd1b5ae7f52fb13f1937270b4242adf5c251647d
Yash Gupta <yashbg7@gmail.com>
2021-05-03 23:42:11 +0530

    Add files

---
+++ server.py
@@ -0,0 +1,24 @@
+from bot import telegram_chatbot
+
+bot = telegram_chatbot('config.cfg')
+
+def make_reply(msg):
+    if msg is not None:
+        reply = 'Okay'
+    return reply
+
+update_id = None
+while True:
+    print('...')
+    updates = bot.get_updates(offset=update_id)
+    updates = updates['result']
+    if updates:
+        for item in updates:
+            update_id = item['update_id']
+            try:
+                message = item['message']['text']
+            except:
+                message = None
+            from_ = item['message']['from']['id']
+            reply = make_reply(message)
+            bot.send_message(reply, from_)
