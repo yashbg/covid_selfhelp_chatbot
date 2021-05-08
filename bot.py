200b858cc6c663f41d95c4400be009ca1d6bd820
Yash Gupta <yashbg7@gmail.com>
2021-05-09 01:55:03 +0530

    Update inline keyboard after callback query

--- bot.py
+++ bot.py
@@ -9,30 +9,39 @@ class telegram_chatbot():
         
     def get_updates(self, offset=None):
         url = self.base + 'getUpdates'
-        params = {'timeout' : 100}
+        params = {'timeout': 100}
         if offset:
             params['offset'] = offset + 1
         r = requests.get(url, params)
         return r.json()
-
-    def send_message_inline(self, msg, option_type, option_list, chat_id):
-        url = self.base + 'sendMessage'
-        inline_keyboard = json.dumps({'inline_keyboard' : [
-            [{'text' : option, 'callback_data' : f'{option_type}:{option}'}] for option in option_list
-        ]})
-        params = {'chat_id' : chat_id, 'text' : msg, 'reply_markup' : inline_keyboard}
-        if msg is not None:
-            requests.post(url, params)
     
     def send_message(self, msg, chat_id):
         url = self.base + 'sendMessage'
-        params = {'chat_id' : chat_id, 'text' : msg}
+        params = {'chat_id': chat_id, 'text': msg}
         if msg is not None:
             requests.post(url, params)
 
+    def send_message_inline(self, msg, option_type, option_list, chat_id):
+        url = self.base + 'sendMessage'
+        inline_keyboard = json.dumps({'inline_keyboard': [
+            [{'text': option, 'callback_data': f'{option_type}:{option}'}] for option in option_list
+        ]})
+        params = {'chat_id': chat_id, 'text': msg, 'reply_markup': inline_keyboard}
+        if msg is not None:
+            requests.post(url, params)
+
+    def edit_message(self, msg, option_type, option_list, chat_id, message_id):
+        url = self.base + 'editMessageText'
+        inline_keyboard = json.dumps({'inline_keyboard': [
+            [{'text': option, 'callback_data': f'{option_type}:{option}'}] for option in option_list
+        ]})
+        params = {'chat_id': chat_id, 'message_id': message_id, 'text': msg, 'reply_markup': inline_keyboard}
+        r = requests.post(url, params)
+        return r.json()
+
     def answer_callback_query(self, callback_id):
         url = self.base + 'answerCallbackQuery'
-        params = {'callback_query_id' : callback_id}
+        params = {'callback_query_id': callback_id}
         requests.post(url, params)
     
     def read_token_from_config_file(self, config):
--- server.py
+++ server.py
@@ -10,23 +10,27 @@ def getInfo(reqmnt, state, city):
     ])
     return df
 
-def answer_query(response_type, reqmnt, state, city, state_list, city_list, chat_id, callback_id):
+def intro():
+    reply = 'Hello. I am the COVID Self Help Chatbot.'
+    return reply
+
+def make_reply(msg):
+    if msg is not None:
+        reply = 'Please select a requirement:'
+    return reply
+
+def answer_query(response_type, reqmnt, state, city, state_list, city_list, chat_id, message_id, callback_id):
     bot.answer_callback_query(callback_id)
     if response_type == 'reqmnt':
         reply = f"Please select the state in which you want {reqmnt}:"
-        bot.send_message_inline(reply, 'state', state_list, chat_id)
+        bot.edit_message(reply, 'state', state_list, chat_id, message_id)
     elif response_type == 'state':
-        reply = f"Please select the city in {state} where you want {reqmnt}: "
-        bot.send_message_inline(reply, 'city', city_list, chat_id)
+        reply = f"Please select the city in {state} where you want {reqmnt}:"
+        bot.edit_message(reply, 'city', city_list, chat_id, message_id)
     elif response_type == 'city':
         info = getInfo(reqmnt, state, city)
         reply = f"Requirement: {info[0][0]}\nState: {info[1][0]}\nCity: {info[2][0]}"
         bot.send_message(reply, chat_id)
-
-def make_reply(msg):
-    if msg is not None:
-        reply = 'Hello. I am the COVID Self Help Chatbot. \nPlease select a requirement: '
-    return reply
 
 reqmnt_list = ['Plasma', 'Oxygen']
 state_list = ['Assam', 'Delhi NCR']
@@ -45,6 +49,7 @@ while True:
                 item = item['callback_query']
                 callback_id = item['id']
                 from_ = item['from']['id']
+                message_id = item['message']['message_id']
                 data = item['data'].split(':')
                 if data[0] == 'reqmnt':
                     reqmnt = data[1]
@@ -52,10 +57,12 @@ while True:
                     state = data[1]
                 elif data[0] == 'city':
                     city = data[1]
-                answer_query(data[0], reqmnt, state, city, state_list, city_list, from_, callback_id)
+                answer_query(data[0], reqmnt, state, city, state_list, city_list, from_, message_id, callback_id)
             elif 'message' in item:
                 item = item['message']
                 from_ = item['from']['id']
                 message = item['text']
+                reply = intro()
+                bot.send_message(reply, from_)
                 reply = make_reply(message)
                 bot.send_message_inline(reply, 'reqmnt', reqmnt_list, from_)
